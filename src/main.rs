a6ef11c3bc630de4e6f5a6f5f5a3dd1db3119d4c
senpai-10 <bmjfdrh@gmail.com>
2022-05-02 07:49:22 +0300

    output time

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,51 @@ fn main() {
         .and_hms(0, 0, 0);
 
     let date_diff = date_component::calculate(&date1, &date2);
+
+    let years = date_diff.year;
+    let months = date_diff.month;
+    let days = date_diff.interval_day;
+
+    let seconds = days * 86_400;
+    let weeks = seconds / 604800;
+    let minutes = seconds / 60;
+    let hours = seconds / 3600;
+
+    if !cli.years
+        && !cli.months
+        && !cli.weeks
+        && !cli.days
+        && !cli.hours
+        && !cli.minutes
+        && !cli.seconds
+    {
+        println!(
+            "years: {}, months: {}, weeks: {}, days: {}, hours: {}, minutes: {}, seconds: {}",
+            years, months, weeks, days, hours, minutes, seconds
+        );
+    }
+
+    if cli.years {
+        println!("{}", years);
+    }
+    if cli.months {
+        println!("{}", months);
+    }
+    if cli.weeks {
+        println!("{}", weeks);
+    }
+    if cli.days {
+        println!("{}", days);
+    }
+    if cli.hours {
+        println!("{}", hours);
+    }
+    if cli.minutes {
+        println!("{}", minutes);
+    }
+    if cli.seconds {
+        println!("{}", seconds);
+    }
 }
 
 struct Date {
